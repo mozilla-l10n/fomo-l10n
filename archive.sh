ce31326f6a60c9a64196e1cc181eb81e05a88170
Lucie Daeye <lucied@mozillafoundation.org>
2020-02-14 16:47:00 +0100

    drop parent directories in archive

--- archive.sh
+++ archive.sh
@@ -2,11 +2,11 @@
 
 set -euo pipefail
 
-echo "Keep the commit hash"
+# Keep the commit hash
 touch ${TRAVIS_COMMIT}
 
 # Selecting only a file in travis doesn't seem to work, using a dir instead
-mkdir to_upload
+mkdir -p to_upload
 
 echo "Archiving files"
-tar czvf ./to_upload/translations.tgz ./dummydjangoproject/translationstests/locale ./dummydjangoproject/translationstests/networkapi/ ./${TRAVIS_COMMIT}
+tar -C dummydjangoproject/translationstests -czvf translations.tgz ./locale ./networkapi ../../${TRAVIS_COMMIT}
