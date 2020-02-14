537de46a4c5166ecacec6c5b2f84ed7f33777b65
Lucie Daeye <lucied@mozillafoundation.org>
2020-02-14 14:51:04 +0100

    use a dir instead of a filter on file

--- archive.sh
+++ archive.sh
@@ -5,5 +5,8 @@ set -euo pipefail
 echo "Keep the commit hash"
 touch ${TRAVIS_COMMIT}
 
+# Selecting only a file in travis doesn't seem to work, using a dir instead
+mkdir to_upload
+
 echo "Archiving files"
-tar czvf translations.tgz ./dummydjangoproject/translationstests/locale ./dummydjangoproject/translationstests/networkapi/ ${TRAVIS_COMMIT}
+tar czvf ./to_upload/translations.tgz ./dummydjangoproject/translationstests/locale ./dummydjangoproject/translationstests/networkapi/ ./${TRAVIS_COMMIT}
