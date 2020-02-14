36c4c95cfc36c4ed077ab11ebc2eed135e4d2358
Lucie Daeye <lucied@mozillafoundation.org>
2020-02-14 14:06:13 +0100

    fix tar tar XD and add flags

--- archive.sh
+++ archive.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-set -e
+set -euo pipefail
 
 echo "Keep the commit hash"
 touch ${TRAVIS_COMMIT}
 
 echo "Archiving files"
-tar tar czvf translations.tgz ./dummydjangoproject/translationstests/locale ./dummydjangoproject/translationstests/networkapi/ ${TRAVIS_COMMIT}
+tar czvf translations.tgz ./dummydjangoproject/translationstests/locale ./dummydjangoproject/translationstests/networkapi/ ${TRAVIS_COMMIT}
