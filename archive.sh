75ee7dd88fa82f6c7665339fb624b87094b129f1
Lucie Daeye <lucied@mozillafoundation.org>
2020-02-19 13:54:30 +0100

    move pt_br files to the pt dir

--- archive.sh
+++ archive.sh
@@ -8,6 +8,18 @@ touch ${TRAVIS_COMMIT}
 # Selecting only a file in travis doesn't seem to work, using a dir instead
 mkdir -p to_upload
 
+# Move the pt_BR files to pt so Django can find them
+# Foundation site
+rm -rf ./foundation/translations/locale/pt/
+mv ./foundation/translations/locale/pt_BR/ ./foundation/translations/locale/pt/
+# Buyers' guide
+rm -rf ./foundation/translations/networkapi/buyersguide/locale/pt/
+mv ./foundation/translations/networkapi/buyersguide/locale/pt_BR/ ./foundation/translations/networkapi/buyersguide/locale/pt/
+# Mozfest
+rm -rf ./foundation/translations/networkapi/mozfest/locale/pt/
+mv ./foundation/translations/networkapi/mozfest/locale/pt_BR/ ./foundation/translations/networkapi/mozfest/locale/pt/
+
 echo "Archiving files"
 tar -C foundation/translations -cvf ./to_upload/translations.tar ./locale ./networkapi
 tar rvf ./to_upload/translations.tar ./${TRAVIS_COMMIT}
+echo "Done!"
