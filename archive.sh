2b44dd524386c59d6c619b0f327db568128a40b4
Théo Chevalier <theo.chevalier11@gmail.com>
2020-03-11 18:46:55 +0000

    Update archive.sh

--- archive.sh
+++ archive.sh
@@ -8,19 +8,6 @@ touch translations_github_commit_${TRAVIS_COMMIT}
 # Selecting only a file in travis doesn't seem to work, using a dir instead
 mkdir -p to_upload
 
-# Move the pt-BR files to pt so Django can find them
-# Foundation site
-rm -rf ./foundation/translations/locale/pt/
-mv ./foundation/translations/locale/pt-BR/ ./foundation/translations/locale/pt/
-# Buyers' guide
-rm -rf ./foundation/translations/networkapi/buyersguide/locale/pt/
-mv ./foundation/translations/networkapi/buyersguide/locale/pt-BR/ ./foundation/translations/networkapi/buyersguide/locale/pt/
-rm -rf ./foundation/translations/networkapi/buyersguide/templates/about/locale/pt/
-mv ./foundation/translations/networkapi/buyersguide/templates/about/locale/pt-BR/ ./foundation/translations/networkapi/buyersguide/templates/about/locale/pt/
-# Mozfest
-rm -rf ./foundation/translations/networkapi/mozfest/locale/pt/
-mv ./foundation/translations/networkapi/mozfest/locale/pt-BR/ ./foundation/translations/networkapi/mozfest/locale/pt/
-
 echo "Archiving files"
 tar -C foundation/translations -cvf ./to_upload/translations.tar ./locale ./networkapi
 tar rvf ./to_upload/translations.tar ./translations_github_commit_${TRAVIS_COMMIT}
