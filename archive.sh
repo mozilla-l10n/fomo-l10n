e148d9cda36bcb46f8720b41a18b28ee9351eb4c
Théo Chevalier <theo.chevalier11@gmail.com>
2021-02-19 14:34:24 +0100

    Keep commit SHA

--- archive.sh
+++ archive.sh
@@ -3,12 +3,12 @@
 set -euo pipefail
 
 # Keep the commit hash
-touch translations_github_commit_${TRAVIS_COMMIT}
+touch translations_github_commit_${GITHUB_SHA}
 
-# Selecting only a file in travis doesn't seem to work, using a dir instead
+# Selecting only a file doesn't seem to work, using a dir instead
 mkdir -p to_upload
 
 echo "Archiving files"
 tar -C foundation/translations -cvf ./to_upload/translations.tar ./locale ./networkapi
-tar rvf ./to_upload/translations.tar ./translations_github_commit_${TRAVIS_COMMIT}
+tar rvf ./to_upload/translations.tar ./translations_github_commit_${GITHUB_SHA}
 echo "Done!"
